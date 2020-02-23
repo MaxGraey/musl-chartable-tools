158cb778cf8f5c272eb43ef0bd8e942954ebf23b
MaxGraey <maxgraey@gmail.com>
2020-02-23 18:48:54 +0200

    reformat

--- ctype/gen_caseignarable.c
+++ ctype/gen_caseignarable.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-	char *set = calloc(0x110000,1);
+int main() {
+	char *set = calloc(0x110000, 1);
 	char table1[0x300];
 	char buf[128], dummy;
 	int a, b;
@@ -11,41 +10,50 @@ int main()
 
 	f = fopen("data/DerivedCoreProperties.txt", "rb");
 	while (fgets(buf, sizeof buf, f)) {
-		if (sscanf(buf, "%x..%x ; Case_Ignorable%c", &a, &b, &dummy)==3)
-			for (; a<=b; a++) set[a]=1;
-		else if (sscanf(buf, "%x ; Case_Ignorable%c", &a, &dummy)==2)
+		if (sscanf(buf, "%x..%x ; Case_Ignorable%c", &a, &b, &dummy) == 3)
+			for (; a <= b; a++)
+				set[a] = 1;
+		else if (sscanf(buf, "%x ; Case_Ignorable%c", &a, &dummy) == 2)
 			set[a] = 1;
 	}
 	fclose(f);
 
 #define STEP 0x100
-	int blocks_needed=0;
-  int i = 0;
-	for (a=0; a<0x20000; a+=STEP,i++) {
-		for (b=0; b<STEP; b++) {
-			if (set[a+b]!=set[a]) break;
-    }
-		printf("%d,", b!=STEP ? 18+blocks_needed++ : 16+!!set[a]);
-    if (i && (((i + 1) % 16) == 0)) printf("\n");
+	int blocks_needed = 0;
+	int i = 0;
+	for (a = 0; a < 0x20000; a += STEP, i++) {
+		for (b = 0; b < STEP; b++) {
+			if (set[a + b] != set[a])
+				break;
+		}
+		printf("%d,", b != STEP ? 18 + blocks_needed++ : 16 + !!set[a]);
+		if (i && (((i + 1) % 16) == 0))
+			printf("\n");
 	}
-	for (a=0; a<32; a++,i++) {
-    printf("0,");
-    if (((i + 1) % 16) == 0) printf("\n");
-  }
-	for (a=0; a<32; a++,i++) {
-    printf("255,");
-    if (((i + 1) % 16) == 0) printf("\n");
-  }
-	for (a=0; a<0x20000; a+=STEP) {
-		for (b=0; b<STEP; b++)
-			if (set[a+b]!=set[a]) break;
-		unsigned x=0;
-		if (b!=STEP) for (b=0; b<STEP; b++,i++) {
-			x=x/2+128*!!set[a+b];
-			if (!(b+1&7)) {
-        printf("%d,", x&255);
-        if (((i + 1) % (16*12)) == 0) printf("\n");
-      }
+	for (a = 0; a < 32; a++, i++) {
+		printf("0,");
+		if (((i + 1) % 16) == 0)
+			printf("\n");
+	}
+	for (a = 0; a < 32; a++, i++) {
+		printf("255,");
+		if (((i + 1) % 16) == 0)
+			printf("\n");
+	}
+	for (a = 0; a < 0x20000; a += STEP)	{
+		for (b = 0; b < STEP; b++)
+			if (set[a + b] != set[a])
+				break;
+		unsigned x = 0;
+		if (b != STEP) {
+			for (b = 0; b < STEP; b++, i++) {
+				x = x / 2 + 128 * !!set[a + b];
+				if (!(b + 1 & 7)) {
+					printf("%d,", x & 255);
+					if (((i + 1) % (16 * 12)) == 0)
+						printf("\n");
+				}
+			}
 		}
 	}
 }
