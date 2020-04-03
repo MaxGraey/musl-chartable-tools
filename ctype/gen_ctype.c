f3c838dfbb4e0637862df64961a051e23c58ac05
MaxGraey <maxgraey@gmail.com>
2020-04-03 09:28:17 +0300

    update

--- ctype/gen_ctype.c
+++ ctype/gen_ctype.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * Usage: (for alphabet table):
- *   gen_ctype a
- */
-
 int main(int argc, char **argv)
 {
 	char *set = calloc(0x110000,1);
@@ -32,7 +27,7 @@ int main(int argc, char **argv)
 	f = fopen("data/DerivedCoreProperties.txt", "rb");
 	while (fgets(buf, sizeof buf, f)) {
 		if (sscanf(buf, "%x..%x ; Alphabetic%c", &a, &b, &dummy)==3)
-			for (; a<=b; a++) set[a] = 1;
+			for (; a<=b; a++) set[a]=1;
 		else if (sscanf(buf, "%x ; Alphabetic%c", &a, &dummy)==2)
 			set[a] = 1;
 	}
@@ -70,38 +65,20 @@ int main(int argc, char **argv)
 
 #define STEP 0x100
 	int blocks_needed=0;
-	int i = 0;
-	for (a=0; a<0x20000; a+=STEP, i++) {
+	for (a=0; a<0x20000; a+=STEP) {
 		for (b=0; b<STEP; b++)
 			if (set[a+b]!=set[a]) break;
-		printf("%d,", b!=STEP ? 18+blocks_needed++ : 16+!!set[a]);
-		if (i && (((i + 1) % 16) == 0))
-		  printf("\n");
+		printf("%d\n", b!=STEP ? 18+blocks_needed++ : 16+!!set[a]);
 	}
-	for (a=0; a<32; a++, i++) {
-		printf("0,");
-		if (((i + 1) % 16) == 0)
-			printf("\n");
-	}
-	for (a=0; a<32; a++, i++) {
-		printf("255,");
-		if (((i + 1) % 16) == 0)
-			printf("\n");
-	}
-	i = 0;
+	for (a=0; a<32; a++) printf("0\n");
+	for (a=0; a<32; a++) printf("255\n");
 	for (a=0; a<0x20000; a+=STEP) {
 		for (b=0; b<STEP; b++)
 			if (set[a+b]!=set[a]) break;
 		unsigned x=0;
-		if (b != STEP) {
-			for (b = 0; b<STEP; b++, i++) {
-				x = x / 2 + 128 * !!set[a + b];
-				if (!(b + 1 & 7)) {
-					printf("%d,", x & 255);
-					if (((i + 1) % (16 * 12)) == 0)
-						printf("\n");
-				}
-			}
+		if (b!=STEP) for (b=0; b<STEP; b++) {
+			x=x/2+128*!!set[a+b];
+			if (!(b+1&7)) printf("%d\n", x&255);
 		}
 	}
 }
